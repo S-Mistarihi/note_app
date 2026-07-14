610ff95dbed6cbd70ebf6c4d9cfc77e02501a210
s.mistarihi <s.mistarihii@gmail.com>
2026-07-14 04:17:27 +0300

    feat: build AddNoteBottomSheetScreen

---
+++ lib/screens/edit_note/edit_note_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/models/note_model.dart';
+
+class EditNoteScreen extends StatelessWidget {
+  final NoteModel note;
+  const EditNoteScreen({super.key, required this.note});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:note_app/core/constants/app_color.dart';
 import 'package:note_app/screens/add_note_sheet/add_note_bottom_sheet_screen.dart';
+import 'package:note_app/screens/home/widget/build_body_with_data.dart';
 import 'package:note_app/screens/home/widget/build_body_without_data.dart';
 import 'package:note_app/screens/home/widget/build_header.dart';
 import 'package:sizer/sizer.dart';
+
+import '../../core/constants/hive_boxes.dart';
+import '../../core/managers/hive_manager.dart';
+import '../../models/note_model.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -17,7 +23,34 @@ class HomeScreen extends StatelessWidget {
           children: [
             SizedBox(height: 6.h),
             BuildHeader(),
-            BuildBodyWithoutData(),
+
+            Expanded(
+              child: ValueListenableBuilder(
+                valueListenable: HiveManager.instance
+                    .box<NoteModel>(HiveBoxes.notes)
+                    .listenable(),
+
+                builder: (context, box, child) {
+                  final notes = box.values.toList();
+
+                  if (notes.isEmpty) {
+                    return BuildBodyWithoutData();
+                  }
+
+                  return ListView.builder(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+
+                    itemCount: notes.length,
+
+                    itemBuilder: (context, index) {
+                      final note = notes[index];
+
+                      return BuildBodyWithData(note: note);
+                    },
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
@@ -62,21 +95,4 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-/*
-* ValueListenableBuilder(
-  valueListenable: box.listenable(),
 
-  builder: (context, box, child) {
-
-    final notes = box.values.toList();
-
-    if (notes.isEmpty) {
-      return const EmptyNotesScreen();
-    }
-
-    return NotesScreen(notes: notes);
-  },
-)
-*
-*
-* */
